34a52287bfb12de4874d5551e5c200352b66d2fb
Json Lee <lijiansong@ict.ac.cn>
2021-03-25 20:50:09 +0800

    update gitignore for tags

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+bazel build --config=cuda //tensorflow/tools/pip_package:build_pip_package
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+pip install /tmp/tensorflow_pkg/tensorflow-2.1.3-cp35-cp35m-linux_x86_64.whl
