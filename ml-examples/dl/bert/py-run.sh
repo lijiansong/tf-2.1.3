75add050372f6278d6830b7b08ea8aa326a7002b
Json Lee <lijiansong@ict.ac.cn>
2021-04-15 15:08:55 +0800

    update bert scripts

--- ml-examples/dl/bert/py-run.sh
+++ ml-examples/dl/bert/py-run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# NOTE: before getting started, please install: py-params>= 0.9.6 and params-flow >= 0.8.0
+# pip install -r requirements.txt
 python gpu_movie_reviews.py --epochs 1 --batch_size 48
